753abc071a385e53e2a3c1d8d2744400cb54fbc6
khaled boussaidi <khaled.boussaidi@codeonce.fr>
2024-06-08 02:19:18 +0100

    grafna+prom to cloud-watch

--- CloudWatch/server.js
+++ CloudWatch/server.js
@@ -6,12 +6,34 @@ const { getMetrics, getNetworkIn , getNetworkOut, getCPUCreditUsage} = require('
 const app = express();
 const AWSController = require('./controllers/AWSController');
 const CloudWatchController = require('./controllers/CloudWatchController');
-
+const awsController = new AWSController();
 const region = 'us-east-1';  // Replace with your AWS region
 const cloudWatchController = new CloudWatchController(region);
 
 app.use(bodyParser.json());
-const awsController = new AWSController();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+const promClient = require('prom-client')// Prometheus setup
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+collectDefaultMetrics();
+
+// Custom metrics
+const httpRequestDurationMicroseconds = new promClient.Histogram({
+    name: 'http_request_duration_ms',
+    help: 'Duration of HTTP requests in ms',
+    labelNames: ['method', 'route', 'status_code'],
+    buckets: [50, 100, 200, 300, 400, 500, 1000] // Buckets for response time from 50ms to 1000ms
+});
+
+// Middleware to measure request durations
+app.use((req, res, next) => {
+    const end = httpRequestDurationMicroseconds.startTimer();
+    res.on('finish', () => {
+        end({ method: req.method, route: req.originalUrl, status_code: res.statusCode });
+    });
+    next();
+});
 
 /******************************************* */
 app.get('/cloudwatch/metrics', getMetrics);
@@ -24,8 +46,13 @@ app.get('/instances', (req, res) => awsController.getAllInstances(req, res));
 
 
 app.get('/instances_id_by_region', (req, res) => cloudWatchController.getAllInstancesByRegion(req, res));
-app.get('/metrics', (req, res) => cloudWatchController.getMetricStatistics(req, res));
+app.get('/metric', (req, res) => cloudWatchController.getMetricStatistics(req, res));
 
+// Expose metrics endpoint
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', promClient.register.contentType);
+  res.end(await promClient.register.metrics());
+});
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
