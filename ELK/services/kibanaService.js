0b8087bf57704213f769e4d8a865d97c8d54e6d8
chaima BOUSSAIDI <120272088+2022boussaidi@users.noreply.github.com>
2024-11-21 11:42:24 +0100

    Update kibanaService.js

--- ELK/services/kibanaService.js
+++ ELK/services/kibanaService.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const BASE_URL = 'https://kibana-lgc-prod.ip-label.net:5601/api/console/proxy';
+const BASE_URL = 'https://kibana-***************:5601/api/console/proxy';
 const AUTH_HEADER = 'Basic ' + Buffer.from('*******************').toString('base64');
 
 const postToKibana = async (path, body) => {
